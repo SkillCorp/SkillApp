42460b693339822284c760c082799f1d1cb9671b
sumarsky <i.dimcevski@gmail.com>
2016-08-12 13:51:15 +0200

    JwtController JwtIssuerOptions Setup

---
+++ src/SkillApp.WebApi/Controllers/JwtController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+using SkillApp.Entities.DTO.Login;
+using SkillApp.WebApi.Options;
+
+namespace SkillApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class JwtController : Controller
+    {
+        private readonly JwtIssuerOptions _issuerOptions;
+
+        public JwtController(IOptions<JwtIssuerOptions> issuerOptions)
+        {
+            _issuerOptions = issuerOptions.Value;
+            ThrowIfInvalidOptions();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Post([FromForm] LoginUser user)
+        {
+            var identity = await GetClaimsIdentity(user);
+            if (identity == null)
+                return BadRequest("Invalid credentials");
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, await _issuerOptions.JtiGenerator()),
+                new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(_issuerOptions.IssuedAt).ToString(), ClaimValueTypes.Integer64),
+                identity.FindFirst("AdminUser")
+            };
+
+            var token = GetToken(claims);
+            var encodedToken = GetEncodedToken(token);
+            return new OkObjectResult(new LoginResponse
+            {
+                access_token = encodedToken,
+                expires_in = (int)_issuerOptions.ValidFor.TotalSeconds
+            });
+        }
+
+        private JwtSecurityToken GetToken(Claim[] claims)
+        {
+            return new JwtSecurityToken(
+                            issuer: _issuerOptions.Issuer,
+                            audience: _issuerOptions.Audience,
+                            notBefore: _issuerOptions.NotBefore,
+                            expires: _issuerOptions.Expiration,
+                            signingCredentials: _issuerOptions.SigningCredentials,
+                            claims: claims);
+        }
+        private static string GetEncodedToken(JwtSecurityToken token)
+        {
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private void ThrowIfInvalidOptions()
+        {
+            if (_issuerOptions == null)
+                throw new ArgumentNullException(nameof(_issuerOptions));
+
+            if (_issuerOptions.ValidFor <= TimeSpan.Zero)
+                throw new ArgumentException("Must be a non-zero TimeSpan.", nameof(JwtIssuerOptions.ValidFor));
+
+            if (_issuerOptions.SigningCredentials == null)
+                throw new ArgumentNullException(nameof(JwtIssuerOptions.SigningCredentials));
+
+            if (_issuerOptions.JtiGenerator == null)
+                throw new ArgumentNullException(nameof(JwtIssuerOptions.JtiGenerator));
+        }
+
+        /// <returns>Date converted to seconds since Unix epoch (Jan 1, 1970, midnight UTC).</returns>
+        private static long ToUnixEpochDate(DateTime date)
+          => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+
+        /// <summary>
+        /// IMAGINE BIG RED WARNING SIGNS HERE!
+        /// You'd want to retrieve claims through your claims provider
+        /// in whatever way suits you, the below is purely for demo purposes!
+        /// </summary>
+        private static Task<ClaimsIdentity> GetClaimsIdentity(LoginUser user)
+        {
+            if (user.UserName == "Igor")
+            {
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(user.UserName, "Token"),
+                  new[]
+                  {
+                    new Claim("AdminUser", "AdminUser")
+                  }));
+            }
+
+            if (user.UserName == "NotIgor")
+            {
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(user.UserName, "Token"),
+                  new Claim[] { }));
+            }
+
+            return NonExistingAccountOrInvalidCredentials();
+        }
+        private static Task<ClaimsIdentity> NonExistingAccountOrInvalidCredentials()
+        {
+            return Task.FromResult<ClaimsIdentity>(null);
+        }
+    }
+}
--- src/SkillApp.WebApi/Startup.cs
+++ src/SkillApp.WebApi/Startup.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
 using SkillApp.Data;
 using SkillApp.DependencyResolution;
 using SkillApp.Entities.Entities;
+using SkillApp.WebApi.Options;
+
 
 namespace SkillApp.WebApi
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        private const string SECRET_KEY = "needtogetthisfromenvironment";
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SECRET_KEY));
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -32,8 +45,6 @@ namespace SkillApp.WebApi
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
@@ -51,22 +62,73 @@ namespace SkillApp.WebApi
             services.AddCustomRepositories();
             services.AddCustomServices();
 
-            services.AddMvc();
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Admin", policy => policy.RequireClaim("AdminUser", "AdminUser"));
+            });
+
+            ConfigureIssuerOptions(services);
+        }
+        private void ConfigureIssuerOptions(IServiceCollection services)
+        {
+            var issuerOptionsConfig = Configuration.GetSection(nameof(JwtIssuerOptions));
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = issuerOptionsConfig[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = issuerOptionsConfig[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseApplicationInsightsRequestTelemetry();
-
             app.UseApplicationInsightsExceptionTelemetry();
 
-            app.UseIdentity();
+            app.UseJwtBearerAuthentication(GetBearerOptions());
 
+            app.UseIdentity();
             app.UseMvc();
+        }
+        private JwtBearerOptions GetBearerOptions()
+        {
+            return new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = GetTokenValidationParameters()
+            };
+        }
+        private TokenValidationParameters GetTokenValidationParameters()
+        {
+            var issuerOptionsConfig = Configuration.GetSection(nameof(JwtIssuerOptions));
+            return new TokenValidationParameters
+            {
+
+                ValidateIssuer = true,
+                ValidIssuer = issuerOptionsConfig[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = true,
+                ValidAudience = issuerOptionsConfig[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                ClockSkew = TimeSpan.Zero
+            };
         }
     }
 }
