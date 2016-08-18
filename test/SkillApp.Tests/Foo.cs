9df4c45a2c78646ab19225dbaef3d73568f399bf
sumarsky <i.dimcevski@gmail.com>
2016-08-18 10:56:37 +0200

    nUnit project added

---
+++ test/SkillApp.Tests/Foo.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace SkillApp.Tests
+{
+    [TestFixture]
+    public class Foo : AssertionHelper
+    {
+        [Test]
+        public void IsNull()
+        {
+            object nada = null;
+
+            // Classic syntax
+            Assert.IsNull(nada);
+
+            // Constraint Syntax
+            Assert.That(nada, Is.Null);
+
+            // Inherited syntax
+            Expect(nada, Null);
+        }
+    }
+}
